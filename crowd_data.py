0bba043b0199c9c3f898d302a44137f75c8bf3c2
Greg Olmschenk <golmschenk@gmail.com>
2016-06-15 15:45:22 -0400

    Update the CrowdData class to work with the latest GoData upgrade

--- crowd_data.py
+++ crowd_data.py
@@ -16,15 +16,11 @@ class CrowdData(GoData):
         self.images_numpy_file_name = 'images.npy'
         self.labels_numpy_file_name = 'densities.npy'
         self.dataset_container = 'file'
-        self.height = 158 // 2  # The height we'll be training on (data will be shrunk if needed).
-        self.width = 238 // 2  # The width we'll be training on (data will be shrunk if needed).
-        self.original_height = 158  # The height of the original data.
-        self.original_width = 238  # The width of the original data.
-        self.image_shape = [self.height, self.width, self.channels]
-        self.label_shape = [self.height, self.width, 1]
+        self.image_height = 158 // 2  # The height we'll be training on (data will be shrunk if needed).
+        self.image_width = 238 // 2  # The width we'll be training on (data will be shrunk if needed).
         self.train_size = 'all'
 
 
 if __name__ == '__main__':
     data = CrowdData()
-    data.numpy_files_to_tfrecords(augment=False)
+    data.numpy_files_to_tfrecords()
