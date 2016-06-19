06f97d4a3e47f79d358074736de07c0032f3d2ca
Greg Olmschenk <golmschenk@gmail.com>
2016-06-19 17:41:04 -0400

    Fix bug where full file path name was not being used

--- crowd_data.py
+++ crowd_data.py
@@ -1,6 +1,7 @@
 """
 Code for managing the crowd data.
 """
+import numpy as np
 import os
 
 from go_data import GoData
@@ -10,6 +11,7 @@ class CrowdData(GoData):
     """
     A class for managing the crowd data.
     """
+
     def __init__(self):
         super().__init__()
 
@@ -34,17 +36,18 @@ class CrowdData(GoData):
             numpy_file_names = [file_name for file_name in file_names if file_name.endswith('.npy')]
             for numpy_file_name in numpy_file_names:
                 if 'image' in numpy_file_name:
-                    if os.path.isfile(numpy_file_name.replace('image', 'density')):
-                        labels_file_name = numpy_file_name.replace('image', 'density')
-                    elif os.path.isfile(numpy_file_name.replace('images', 'densities')):
-                        labels_file_name = numpy_file_name.replace('images', 'densities')
-                    elif os.path.isfile(numpy_file_name.replace('image', 'label')):
-                        labels_file_name = numpy_file_name.replace('image', 'label')
-                    elif os.path.isfile(numpy_file_name.replace('images', 'labels')):
-                        labels_file_name = numpy_file_name.replace('images', 'labels')
+                    images_path = os.path.abspath(os.path.join(file_directory, numpy_file_name))
+                    if os.path.isfile(images_path.replace('image', 'density')):
+                        labels_path = images_path.replace('image', 'density')
+                    elif os.path.isfile(images_path.replace('images', 'densities')):
+                        labels_path = images_path.replace('images', 'densities')
+                    elif os.path.isfile(images_path.replace('image', 'label')):
+                        labels_path = images_path.replace('image', 'label')
+                    elif os.path.isfile(images_path.replace('images', 'labels')):
+                        labels_path = images_path.replace('images', 'labels')
                     else:
                         continue
-                    import_file_paths.append((numpy_file_name, labels_file_name))
+                    import_file_paths.append((images_path, labels_path))
         return import_file_paths
 
     def obtain_export_name(self, import_file_path):
@@ -60,7 +63,26 @@ class CrowdData(GoData):
         export_name = os.path.splitext(os.path.basename(image_file_path))[0]
         self.data_name = export_name.replace('images', 'data_pair').replace('image', 'data_pair')
 
+    def import_file(self, file_path):
+        """
+        Import the data.
+        Should be overwritten by subclasses.
+
+        :param file_path: The file path of the file to be imported.
+        :type file_path: str | (str, str)
+        """
+        self.import_numpy_pair_files(file_path)
+
+    def import_numpy_pair_files(self, file_path_pair):
+        """
+        Imports the images and labels from the file path pair.
+
+        :param file_path_pair: The pair of file paths.
+        :type file_path_pair: (str, str)
+        """
+        self.images = np.load(file_path_pair[0])
+        self.labels = np.load(file_path_pair[1])
 
 if __name__ == '__main__':
     data = CrowdData()
-    data.numpy_files_to_tfrecords()
+    data.generate_all_tfrecords()
