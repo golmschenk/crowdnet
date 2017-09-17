80cf9cb15f2b296739bdbc645d0c1bc9a73bc593
Greg Olmschenk <golmschenk@gmail.com>
2017-09-17 14:57:19 -0400

    Add perspectives to database conversion

--- database.py
+++ database.py
@@ -27,24 +27,29 @@ def data_type_block_database_from_structured_database(structured_database_direct
         images = None
         labels = None
         rois = None
+        perspectives = None
         for camera in cameras:
             camera_directory = os.path.join(structured_database_directory, camera)
             camera_images = np.load(os.path.join(camera_directory, 'images.npy'))
             camera_labels = np.load(os.path.join(camera_directory, 'labels.npy'))
             camera_roi = np.load(os.path.join(camera_directory, 'roi.npy'))
+            camera_perspective = np.load(os.path.join(camera_directory, 'perspective.npy'))
             if images is None:
                 images = camera_images
                 labels = camera_labels
                 rois = np.tile(camera_roi, (labels.shape[0], 1, 1))
+                perspectives = np.tile(camera_perspective, (labels.shape[0], 1, 1))
             else:
                 images = np.concatenate((images, camera_images), axis=0)
                 labels = np.concatenate((labels, camera_labels), axis=0)
                 rois = np.concatenate((np.tile(camera_roi, (camera_labels.shape[0], 1, 1)), rois), axis=0)
+                perspectives = np.concatenate((np.tile(camera_perspective, (camera_labels.shape[0], 1, 1)), rois), axis=0)
         dataset_directory = os.path.join(data_type_database_directory, data_type)
         os.makedirs(dataset_directory, exist_ok=True)
         np.save(os.path.join(dataset_directory, 'images.npy'), images)
         np.save(os.path.join(dataset_directory, 'labels.npy'), labels)
         np.save(os.path.join(dataset_directory, 'rois.npy'), rois)
+        np.save(os.path.join(dataset_directory, 'perspectives.npy'), perspectives)
 
 
 data_type_block_database_from_structured_database('../storage/data/world_expo_database',
