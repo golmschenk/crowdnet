Fix numerical precision bug where the summing after resizing can be small enough to round to zero

transforms.py
@@ -183,8 +183,8 @@ class PatchAndRescale:
         image = scipy.misc.imresize(patch.image, self.image_scaled_size)
         original_label_sum = np.sum(patch.label)
         label = scipy.misc.imresize(patch.label, self.label_scaled_size, mode='F')
-        if original_label_sum != 0:
-            unnormalized_label_sum = np.sum(label)
+        unnormalized_label_sum = np.sum(label)
+        if unnormalized_label_sum != 0:
             label = (label / unnormalized_label_sum) * original_label_sum
         roi = scipy.misc.imresize(patch.roi, self.image_scaled_size, mode='F') > 0.5
         return CrowdExample(image=image, label=label, roi=roi)