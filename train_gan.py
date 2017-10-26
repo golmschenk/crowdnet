5436aac33c42f4f1850a29b58d16e3a46b532b13
Greg Olmschenk <golmschenk@gmail.com>
2017-10-26 11:29:57 -0400

    Fix running example count to reset on summary

--- train_gan.py
+++ train_gan.py
@@ -141,6 +141,7 @@ while epoch < settings.number_of_epochs:
                 mean_scalar = running_scalar / running_example_count
                 summary_writer.add_scalar(name, mean_scalar, global_step=step)
                 running_scalars[name] = 0
+            running_example_count = 0
             for validation_examples in train_dataset_loader:
                 images, labels, _ = validation_examples
                 images, labels = Variable(gpu(images)), Variable(gpu(labels))
