dc3692b1d2518a9f76aeda4be35be69251d75714
Greg Olmschenk <golmschenk@gmail.com>
2017-11-21 21:37:39 -0500

    Return to paper state networks

--- train_cnn.py
+++ train_cnn.py
@@ -97,92 +97,14 @@ def train(settings=None):
                                                                 ).mean().data[0]
             running_scalars['Predictor/Count ME'] += (predictor_predicted_counts - labels.sum(1).sum(1)).mean().data[0]
             running_scalars['Predictor/Exponent'] += P.exponent.data[0]
-            # Unlabeled image discriminator processing.
-            unlabeled_images, _, _ = unlabeled_examples
-            unlabeled_images = Variable(gpu(unlabeled_images))
-            unlabeled_predicted_labels, unlabeled_predicted_counts = D(unlabeled_images)
-            label_count_mean = labels.sum(1).sum(1).mean()
-            count_mean = labels.sum(1).sum(1).mean()
-            unlabeled_predicted_count_mean = unlabeled_predicted_counts.mean()
-            unlabeled_predicted_label_count_mean = unlabeled_predicted_labels.sum(1).sum(1).mean()
-            beta = 2.0
-            # noinspection PyArgumentList
-            zero = Variable(gpu(torch.FloatTensor([0])))
-            unlabeled_count_loss_min = torch.max(zero, count_mean / beta - unlabeled_predicted_count_mean)
-            unlabeled_count_loss_max = torch.max(zero, unlabeled_predicted_count_mean - count_mean * beta)
-            unlabeled_label_loss_min = torch.max(zero,
-                                                 label_count_mean / beta - unlabeled_predicted_label_count_mean)
-            unlabeled_label_loss_max = torch.max(zero,
-                                                 unlabeled_predicted_label_count_mean - label_count_mean * beta)
-            unlabeled_density_loss = unlabeled_label_loss_max + unlabeled_label_loss_min
-            unlabeled_count_loss = unlabeled_count_loss_max + unlabeled_count_loss_min
-            unlabeled_loss = unlabeled_count_loss + (unlabeled_density_loss * 10)
-            running_scalars['Unlabeled/Count ME'] += (unlabeled_predicted_count_mean - count_mean).data[0]
-            running_scalars['Unlabeled/Count'] += unlabeled_predicted_count_mean.data[0]
-            running_scalars['Unlabeled/Loss'] += unlabeled_loss.data[0]
-            unlabeled_loss.backward()
-            # Fake image discriminator processing.
-            current_batch_size = images.data.shape[0]
-            z = torch.randn(current_batch_size, 100)
-            fake_images = G(Variable(gpu(z)))
-            fake_predicted_labels, fake_predicted_counts = D(fake_images)
-            fake_density_loss = torch.abs(fake_predicted_labels).pow(settings.loss_order).sum(1).sum(1).mean()
-            fake_count_loss = torch.abs(fake_predicted_counts).pow(settings.loss_order).mean()
-            fake_mean_count = fake_predicted_counts.mean()
-            fake_discriminator_loss = fake_count_loss + (fake_density_loss * 10)
-            running_scalars['Fake/Count'] += fake_mean_count.data[0]
-            running_scalars['Fake/Loss'] += fake_discriminator_loss.data[0]
-            fake_discriminator_loss.backward(retain_graph=True)
-            # Gradient penalty.
-            alpha = Variable(gpu(torch.rand(3, current_batch_size, 1, 1, 1)))
-            alpha = alpha / alpha.sum(0)
-            interpolates = alpha[0] * images + alpha[1] * unlabeled_images + alpha[2] * fake_images
-            interpolates_labels, interpolates_counts = D(interpolates)
-            density_gradients = torch.autograd.grad(outputs=interpolates_labels, inputs=interpolates,
-                                                    grad_outputs=gpu(torch.ones(interpolates_labels.size())),
-                                                    create_graph=True, retain_graph=True, only_inputs=True)[0]
-            density_gradients = density_gradients.view(current_batch_size, -1)
-            density_gradient_penalty = ((density_gradients.norm(2, dim=1) - 1) ** 2).mean() * 10
-            count_gradients = torch.autograd.grad(outputs=interpolates_counts, inputs=interpolates,
-                                                  grad_outputs=gpu(torch.ones(interpolates_counts.size())),
-                                                  create_graph=True, retain_graph=True, only_inputs=True)[0]
-            count_gradients = count_gradients.view(current_batch_size, -1)
-            count_gradients_penalty = ((count_gradients.norm(2, dim=1) - 1) ** 2).mean() * 10
-            gradient_penalty = count_gradients_penalty + density_gradient_penalty * 10
-            gradient_penalty.backward()
             # Discriminator update.
             discriminator_optimizer.step()
-            # Generator image processing.
-            generator_optimizer.zero_grad()
-            z = torch.randn(current_batch_size, 100)
-            fake_images = G(Variable(gpu(z)))
-            _, _ = D(fake_images)  # Produces feature layer for next line.
-            fake_feature_layer = D.feature_layer
-            detached_predicted_counts = predicted_counts.detach()
-            detached_predicted_labels = predicted_labels.detach()
-            detached_real_feature_layer = real_feature_layer.detach()
-            # noinspection PyArgumentList
-            epsilon = Variable(gpu(torch.FloatTensor([1e-10])))
-            count_weights = (detached_predicted_counts / torch.max(detached_predicted_counts.sum(),
-                                                                   epsilon)).view(-1, 1, 1, 1)
-            labels_weights = (detached_predicted_labels.sum(1).sum(1) / torch.max(detached_predicted_labels.sum(),
-                                                                                  epsilon)).view(-1, 1, 1, 1)
-            feature_weights = (count_weights + (labels_weights * 10)) / 11
-            weighted_real_feature_layer = feature_weights * detached_real_feature_layer
-            generator_loss = (weighted_real_feature_layer.mean(0) - fake_feature_layer.mean(0)).abs().sum()
-            running_scalars['Generator/Loss'] += generator_loss.data[0]
-            # Generator update.
-            if step % 5 == 0:
-                generator_loss.backward()
-                generator_optimizer.step()
 
             running_example_count += images.size()[0]
             if step % settings.summary_step_period == 0 and step != 0:
                 comparison_image = viewer.create_crowd_images_comparison_grid(cpu(images), cpu(labels),
                                                                               cpu(predicted_labels))
                 summary_writer.add_image('Comparison', comparison_image, global_step=step)
-                fake_images_image = torchvision.utils.make_grid(fake_images.data[:9], nrow=3)
-                summary_writer.add_image('Fake', fake_images_image, global_step=step)
                 mean_loss = running_scalars['Labeled/Loss'] / running_example_count
                 print('[Epoch: {}, Step: {}] Loss: {:g}'.format(epoch, step, mean_loss))
                 for name, running_scalar in running_scalars.items():
